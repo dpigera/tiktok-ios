2fc6c1bf322b61ee76103375557c5e612845bf85
dpigera <dpigera@gmail.com>
2025-02-04 09:02:15 -0600

    Initial Commit

---
+++ TikTokLogin1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokLogin1
+//
+//  Created by Devin Pigera on 2/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
